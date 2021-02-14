410848b809a2b829db7b610677952b4cdc1aff22
Vienio99 <kyve96@gmail.com>
2021-02-14 11:04:52 +0100

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -30,9 +30,16 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
+
+        inputbox.send_keys('Buy peacock feathers') 
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
         inputbox.send_keys('Use peacock feathers to make a fly') 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
--- lists/tests.py
+++ lists/tests.py
@@ -12,18 +12,39 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         
         html = response.content.decode('utf8')
+
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
     def test_uses_home_template(self):
         response = self.client.get('/')
+
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_post_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        new_item = Item.objects.first()
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
