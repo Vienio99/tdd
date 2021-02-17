ab8cded7930765d01c0ff56d97a9af0aed6a0c1a
Vienio99 <kyve96@gmail.com>
2021-02-17 17:44:59 +0100

    introduced new view and template for making lists

--- lists/tests.py
+++ lists/tests.py
@@ -22,19 +22,17 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response, 'home.html')
 
+class NewListTest(TestCase):
+
     def test_can_save_a_post_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_item = Item.objects.first()
-
         self.assertEqual(Item.objects.count(), 1)
         self.assertEqual(new_item.text, 'A new list item')
     
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
