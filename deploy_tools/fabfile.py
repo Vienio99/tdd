d641956584caf8a56d6cb5329ed8e3c47b4e6550
Vienio99 <kyve96@gmail.com>
2021-03-01 19:22:01 +0100

    Add fabfile and changed gunicorn and nginx conf files for automated deploys

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,59 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+from fabric.context_managers import prefix
+
+REPO_URL = 'https://github.com/Vienio99/tdd'
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_lastest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+def _get_lastest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+def _update_virtualenv():
+    profile = run('cat ~/.bash_profile')
+    if 'export WORKON_HOME=~/Envs' not in profile:
+        append('~/.bash_profile', """
+        export WORKON_HOME=~/Envs
+        export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.8
+        export VIRTUALENVWRAPPER_VIRTUALENV=/home/elspeth/.local/bin/virtualenv
+        source /home/elspeth/.local/bin/virtualenvwrapper.sh
+        """)
+
+    if not exists('/home/elspeth/.local/bin/virtualenv'):
+        run('pip3 install virtualenv')
+    if not exists('/home/elspeth/.local/bin/virtualenvwrapper.sh'):
+        run('pip3 install virtualenvwrapper')
+    run("mkdir -p '$WORKON_HOME'")
+
+    if not exists(f'/home/elspeth/Envs/{env.host}'):
+        run(f'mkvirtualenv {env.host}')
+    with prefix(f'workon {env.host}'):
+        run('pip3 install -r requirements.txt')
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+def _update_static_files():
+    run('python3 manage.py collectstatic --noinput')
+
+def _update_database():
+    run('python3 manage.py migrate --noinput')
